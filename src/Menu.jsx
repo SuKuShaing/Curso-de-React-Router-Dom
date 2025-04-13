a42612b1f107fcce542af946577dd08295d2e72c
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-04-13 15:30:59 -0400

    Rendirizado condicional al agregar autenticación al menú de navegación y definir rutas privadas y públicas

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import { useAuth } from "./auth";
 
 function Menu() {
-    const activeStyle = ({ isActive }) => ({
-        color: isActive ? "red" : "black",
-        textDecoration: isActive ? "none" : "underline",
-    });
+	const activeStyle = ({ isActive }) => ({
+		color: isActive ? "red" : "black",
+		textDecoration: isActive ? "none" : "underline",
+	});
 
-    const routes = []
-    routes.push({ to: "/", text: "Home" });
-    routes.push({ to: "/blog", text: "Blog" });
-    routes.push({ to: "/profile", text: "Profile" });
-    routes.push({ to: "/login", text: "Login" });
-    routes.push({ to: "/logout", text: "Logout" });
+	const routes = [];
+	routes.push({ private: false, to: "/", text: "Home" });
+	routes.push({ private: false, to: "/blog", text: "Blog" });
+	routes.push({ private: true, to: "/profile", text: "Profile" });
+	routes.push({ private: false, publicOnly: true, to: "/login", text: "Login" });
+	routes.push({ private: true, to: "/logout", text: "Logout" });
 
-    return (
-        <nav className="menu">
-            <ul>
-                {/* <li>
+    const auth = useAuth();
+
+	return (
+		<nav className="menu">
+			<ul>
+				{/* <li>
                     <Link to="/">Home</Link>
                 </li>
                 <li>
@@ -27,11 +30,11 @@ function Menu() {
                     <Link to="/profile">Profile</Link>
                 </li> */}
 
-                {/* 
+				{/* 
                     La diferencia entre Link y NavLink es que NavLink permite aplicar estilos a los enlaces activos.
                 */}
 
-                {/* <li>
+				{/* <li>
                     <NavLink 
                         // className={({ isActive }) => (isActive ? "active" : "")}
                         // style={({ isActive }) => (isActive ? { color: "red" } : {})}
@@ -49,17 +52,22 @@ function Menu() {
                         to="/profile">Profile</NavLink>
                 </li> */}
 
-                {routes.map((route) => (
-                    <li key={route.to}>
-                        <NavLink
-                            style={activeStyle}
-                            to={route.to}>{route.text}</NavLink>
-                    </li>
-                ))}
-            </ul>
-            <hr />
-        </nav>
-    );
+				{routes.map((route) => {
+                    if (route.private && !auth.user) return null
+                    if (route.publicOnly && auth.user) return null
+                    
+                    return (
+                        <li key={route.to}>
+                            <NavLink style={activeStyle} to={route.to}>
+                                {route.text}
+                            </NavLink>
+                        </li>
+                    )
+                })}
+			</ul>
+			<hr />
+		</nav>
+	);
 }
 
 export default Menu;
